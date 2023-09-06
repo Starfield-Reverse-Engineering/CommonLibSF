c0bb1fb785d3abcf11e97c4319ff102c94085a35
clang-format <gottyduke@users.noreply.github.com>
2023-09-06 03:19:14 +0000

    fix: redo workflow

--- CommonLibSF/include/RE/Starfield.h
+++ CommonLibSF/include/RE/Starfield.h
@@ -2,8 +2,8 @@
 
 #include "SFSE/Impl/PCH.h"
 
-#include "RE/Offsets_NiRTTI.h" 
-#include "RE/Offsets_RTTI.h" 
-#include "RE/Offsets_VTABLE.h" 
-#include "RE/Offsets.h" 
+#include "RE/Offsets.h"
+#include "RE/Offsets_NiRTTI.h"
+#include "RE/Offsets_RTTI.h"
+#include "RE/Offsets_VTABLE.h"
 #include "RE/RTTI.h"
