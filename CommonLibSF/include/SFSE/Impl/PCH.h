476551588a89811e8f062a0a9869cf15eece8514
maintenance <gottyduke@users.noreply.github.com>
2023-09-07 04:40:27 +0000

    chore: maintenance

--- CommonLibSF/include/SFSE/Impl/PCH.h
+++ CommonLibSF/include/SFSE/Impl/PCH.h
@@ -666,7 +666,7 @@ namespace SFSE
 		}
 
 		[[noreturn]] inline void report_and_fail(std::string_view a_msg,
-			std::source_location a_loc = std::source_location::current())
+			std::source_location                                  a_loc = std::source_location::current())
 		{
 			report_and_error(a_msg, true, a_loc);
 			std::unreachable();
