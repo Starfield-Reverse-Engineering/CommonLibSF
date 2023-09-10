83b0b555e6bec74345af2830e902c99c14d9394b
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-09-10 22:59:50 +0530

    feat: add RTTI, VTABLES

--- CommonLibSF/include/RE/RTTI.h
+++ CommonLibSF/include/RE/RTTI.h
@@ -159,7 +159,7 @@ namespace RE
 	inline void* RTDynamicCast(void* a_inptr, std::int32_t a_vfDelta, void* a_srcType, void* a_targetType, std::int32_t a_isReference)
 	{
 		using func_t = decltype(&RTDynamicCast);
-		REL::Relocation<func_t> func{ 0x0 };
+		REL::Relocation<func_t> func{ REL::Offset(0x034C6C30) };
 		return func(a_inptr, a_vfDelta, a_srcType, a_targetType, a_isReference);
 	}
 
