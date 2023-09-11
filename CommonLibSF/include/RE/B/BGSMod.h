bc62be74b125578b607dacc7cfe7c65eecdf5692
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 16:18:35 +0800

    feat: `BGSMod`

---
+++ CommonLibSF/include/RE/B/BGSMod.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+
+namespace RE
+{
+	namespace Template
+	{
+		class Items :
+			public BaseFormComponent  // 00
+		{
+		public:
+			virtual ~Items();
+
+			// add
+			virtual void Unk_08();  // 08
+			virtual void Unk_09();  // 09
+			virtual void Unk_0A();  // 0A
+
+			// members
+			void* unk08;  // 08
+			void* unk10;  // 10
+			void* unk18;  // 18
+		};
+		static_assert(sizeof(Items) == 0x20);
+	}
+}
