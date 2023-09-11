134a2a69949f6a4ecbb757310dd4d55cdfa97285
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 18:41:05 +0800

    feat: `BSIntrusiveRefCounted`

---
+++ CommonLibSF/include/RE/B/BSIntrusiveRefCounted.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace RE
+{
+	struct BSIntrusiveRefCounted
+	{
+	public:
+		TES_RTTI(BSIntrusiveRefCounted);
+
+		volatile mutable std::uint32_t refCount{ 0 };  // 00
+		std::uint32_t                  unk04;          // 04
+	};
+	static_assert(sizeof(BSIntrusiveRefCounted) == 0x08);
+}
+
