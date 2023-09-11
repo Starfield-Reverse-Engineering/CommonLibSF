459f5f3b5159db12612c07d04bedf727e65b8a80
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 17:47:20 +0800

    feat: `ActorValues`

---
+++ CommonLibSF/include/RE/A/ActorValues.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace RE
+{
+	enum class ACTOR_VALUE_MODIFIER
+	{
+		kPermanent = 0,
+		kTemporary = 1,
+		kDamage = 2,
+
+		kTotal = 3
+	};
+}
+
---
+++ CommonLibSF/src/RE/A/ActorValues.cpp
@@ -0,0 +1,5 @@
+#include "RE/A/ActorValues.h"
+namespace RE
+{
+}
+
