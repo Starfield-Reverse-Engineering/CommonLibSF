ce23f4fe3e78f55928151194c9668b0438414ae4
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 17:27:46 +0800

    feat: `TESSpellList`

---
+++ CommonLibSF/include/RE/T/TESSpellList.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+
+namespace RE
+{
+	class TESSpellList : public BaseFormComponent
+	{
+	public:
+		TES_RTTI_VTABLE(TESSpellList);
+
+		// members
+		std::uint32_t unk08;  // 08
+		std::uint32_t unk0C;  // 0C
+		std::uint64_t unk10;  // 10
+	};
+	static_assert(sizeof(TESSpellList) == 0x18);
+}
