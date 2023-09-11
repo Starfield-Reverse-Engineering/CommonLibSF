cddd9f903379361398199087ee3851a0da787c56
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 20:08:23 +0800

    fix: `BGSMod`

--- CommonLibSF/include/RE/B/BGSMod.h
+++ CommonLibSF/include/RE/B/BGSMod.h
@@ -4,26 +4,29 @@
 
 namespace RE
 {
-	namespace Template
+	namespace BGSMod
 	{
-		class Items :
-			public BaseFormComponent  // 00
+		namespace Template
 		{
-		public:
-			TES_RTTI_VTABLE(BGSMod__Template__Items);
+			class Items :
+				public BaseFormComponent  // 00
+			{
+			public:
+				TES_RTTI_VTABLE(BGSMod__Template__Items);
 
-			virtual ~Items();
+				virtual ~Items();
 
-			// add
-			virtual void Unk_08();  // 08
-			virtual void Unk_09();  // 09
-			virtual void Unk_0A();  // 0A
+				// add
+				virtual void Unk_08();  // 08
+				virtual void Unk_09();  // 09
+				virtual void Unk_0A();  // 0A
 
-			// members
-			void* unk08;  // 08
-			void* unk10;  // 10
-			void* unk18;  // 18
-		};
-		static_assert(sizeof(Items) == 0x20);
+				// members
+				void* unk08;  // 08
+				void* unk10;  // 10
+				void* unk18;  // 18
+			};
+			static_assert(sizeof(Items) == 0x20);
+		}
 	}
 }
