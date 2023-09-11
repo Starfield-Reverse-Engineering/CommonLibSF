2e645cabd717b5ab4f03a7868897522197a52de7
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:16:09 +0800

    feat: `BGSPerkRankArray`

---
+++ CommonLibSF/include/RE/B/BGSPerkRankArray.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+
+namespace RE
+{
+	class BGSPerk;
+
+	struct PerkRankData
+	{
+		// members
+		BGSPerk*    perk;         // 00
+		std::int8_t currentRank;  // 08
+	};
+	static_assert(sizeof(PerkRankData) == 0x10);
+
+	class BGSPerkRankArray : public BaseFormComponent
+	{
+	public:
+		TES_RTTI(BGSPerkRankArray);
+
+		// members
+		PerkRankData* perks;      // 08
+		std::uint32_t perkCount;  // 10
+	};
+	static_assert(sizeof(BGSPerkRankArray) == 0x18);
+}
+
---
+++ CommonLibSF/src/RE/B/BGSPerkRankArray.cpp
@@ -0,0 +1,2 @@
+#include "RE/B/BGSPerkRankArray.h"
+
