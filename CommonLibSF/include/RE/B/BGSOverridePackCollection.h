d3b018789ef22cff2fc7a71aab1d6a3026dc55ac
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:25:40 +0800

    feat: `BGSOverridePackCollection`

---
+++ CommonLibSF/include/RE/B/BGSOverridePackCollection.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+#include "RE/B/BGSListForm.h"
+
+namespace RE
+{
+	class BGSOverridePackCollection : public BaseFormComponent
+	{
+	public:
+		TES_RTTI_VTABLE(BGSOverridePackCollection);
+
+		// members
+		BGSListForm* unk08;
+		BGSListForm* unk10;
+		BGSListForm* unk18;
+		BGSListForm* unk20;
+		BGSListForm* unk28;
+		BGSListForm* unk30;
+		BGSListForm* unk38;
+		BGSListForm* unk40;
+	};
+	static_assert(sizeof(BGSOverridePackCollection) == 0x48);
+}
+
