689387beecd6077379860729e5aa3dfffe42fff4
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:34:45 +0800

    feat: `TESBoundAnimObject`

---
+++ CommonLibSF/include/RE/T/TESBoundAnimObject.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "RE/T/TESBoundObject.h"
+
+namespace RE
+{
+	class TESBoundAnimObject : public TESBoundObject
+	{
+	public:
+		TES_RTTI_VTABLE(TESBoundAnimObject);
+
+		virtual ~TESBoundAnimObject();
+
+		// add
+		virtual void Unk_80();  // 80
+		virtual void Unk_81();  // 81
+
+		// members
+		std::uint64_t unkF0;   // F0
+		std::uint64_t unkF8;   // F8
+		std::uint64_t unk100;  // 100
+		std::uint64_t unk108;  // 108
+		std::uint64_t unk110;  // 110
+	};
+	static_assert(sizeof(TESBoundAnimObject) == 0x118);
+}
+
