15a0e9d5c70dbbe1d5dfaf31fe4073d0f4326204
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 16:04:54 +0800

    feat: `BGSSnapTemplateComponent`

---
+++ CommonLibSF/include/RE/B/BGSSnapTemplateComponent.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+
+namespace RE
+{
+	class BGSSnapTemplateComponent : public BaseFormComponent
+	{
+	public:
+		virtual ~BGSSnapTemplateComponent();
+
+		// add
+		virtual void Unk_08();  // 08
+		virtual void Unk_09();  // 09
+		virtual void Unk_0A();  // 0A
+
+		// members
+		std::uint64_t unk08;  // 08
+		std::uint64_t unk10;  // 10
+	};
+	static_assert(sizeof(BGSSnapTemplateComponent) == 0x18);
+}
+
