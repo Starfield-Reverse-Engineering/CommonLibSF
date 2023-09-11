61d7b8132d1fef1bd4df0415f3eb40a8b3c012d3
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:50:04 +0800

    feat: `TESObjectCELL`

---
+++ CommonLibSF/include/RE/T/TESObjectCELL.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "RE/T/TESForm.h"
+#include "RE/T/TESFullName.h"
+
+namespace RE
+{
+	class TESObjectCELL :
+		public TESForm,     // 00
+		public TESFullName  // 20
+	{
+	public:
+		TES_RTTI_VTABLE(TESObjectCELL);
+
+		//
+	};
+}
