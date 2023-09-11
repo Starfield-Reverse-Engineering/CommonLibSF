66569e036b8f5fe86decd1de8d8c5a8b1de4299d
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:05:41 +0800

    feat: `BGSKeywordForm`

---
+++ CommonLibSF/include/RE/B/BGSKeywordForm.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+#include "RE/I/IKeywordFormBase.h"
+
+namespace RE
+{
+	class BGSKeywordForm :
+		public BaseFormComponent, // 00
+		public IKeywordFormBase   // 08
+	{
+	public:
+		TES_RTTI_VTABLE(BGSKeywordForm);
+
+		// members
+		std::uint32_t unk10;
+		std::uint32_t unk14;
+		std::uint64_t unk18;
+		std::uint32_t unk20;
+		std::uint32_t unk24;
+		std::uint64_t unk28;
+	};
+	static_assert(sizeof(BGSKeywordForm) == 0x30);
+}
+
---
+++ CommonLibSF/src/RE/B/BGSKeywordForm.cpp
@@ -0,0 +1,5 @@
+#include "RE/B/BGSKeywordForm.h"
+namespace RE
+{
+}
+
