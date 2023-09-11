3275763264c82c8eb8d9858768dbfff2b3a8091b
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 18:48:49 +0800

    feat: `TBO_InstanceData`

---
+++ CommonLibSF/include/RE/T/TBO_InstanceData.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "RE/B/BSIntrusiveRefCounted.h"
+
+namespace RE
+{
+	class TBO_InstanceData :
+		public BSIntrusiveRefCounted // 08
+	{
+	public:
+		TES_RTTI_VTABLE(TBO_InstanceData);
+
+		virtual ~TBO_InstanceData() = default;  // 00
+	};
+}
+
