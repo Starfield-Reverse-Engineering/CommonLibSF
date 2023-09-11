ff8fa13fe39a426e15bca891499d51121637460e
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:21:09 +0800

    feat: `TESRaceForm`

---
+++ CommonLibSF/include/RE/T/TESRaceForm.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+#include "RE/T/TESRace.h"
+
+namespace RE
+{
+	class TESRaceForm : public BaseFormComponent
+	{
+	public:
+		TES_RTTI_VTABLE(TESRaceForm);
+
+		// members
+		TESRace* formRace;  // 08
+	};
+	static_assert(sizeof(TESRaceForm) == 0x10);
+}
+
