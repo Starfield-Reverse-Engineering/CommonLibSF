8c266d65338d4da9885d6c2d623e5d2687495630
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:45:34 +0800

    feat: `TESTopicInfo`

---
+++ CommonLibSF/include/RE/T/TESTopicInfo.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "RE/T/TESForm.h"
+
+namespace RE
+{
+	class TESTopicInfo : public TESForm
+	{
+	public:
+		TES_RTTI_VTABLE(TESTopicInfo);
+
+		// members
+		std::uint64_t unk38; // 38
+	};
+}
+
