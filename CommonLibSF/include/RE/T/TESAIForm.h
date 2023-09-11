aaa2c8cf028c3787f45fd1b21e2f527862d4c465
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 17:33:58 +0800

    feat: `TESAIForm`

---
+++ CommonLibSF/include/RE/T/TESAIForm.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+
+namespace RE
+{
+	class PackageList
+	{
+	public:
+		TES_RTTI_VTABLE(PackageList);
+
+		virtual void Unk_00();
+
+		// members
+		std::uint64_t unk08;  // 08
+		std::uint64_t unk10;  // 10
+		std::uint64_t unk18;  // 18
+		std::uint64_t unk20;  // 20
+	};
+	static_assert(sizeof(PackageList) == 0x28);
+
+	struct alignas(0x4) AIDATA_GAME
+	{
+		// members
+		std::uint16_t unk00;             // 00
+		std::uint8_t  unk02;             // 02
+		std::uint8_t  pad03;             // 03
+		std::uint16_t aggroRadius[4];    // 04
+		std::uint16_t pad0A;             // 0A
+		std::uint8_t  unk0C;             // 0C
+	};
+	static_assert(sizeof(AIDATA_GAME) == 0x10);
+
+	class TESAIForm : public BaseFormComponent
+	{
+	public:
+		AIDATA_GAME aiData;      // 08
+		PackageList aiPackList;  // 18
+	};
+	static_assert(sizeof(TESAIForm) == 0x40);
+}
+
