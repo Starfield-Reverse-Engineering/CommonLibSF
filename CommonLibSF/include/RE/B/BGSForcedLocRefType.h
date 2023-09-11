2fd2f03519cc9457f07b61f9fd60742fe781ca2b
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:27:47 +0800

    feat: `BGSForcedLocRefType`

---
+++ CommonLibSF/include/RE/B/BGSForcedLocRefType.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+
+namespace RE
+{
+	class BGSForcedLocRefType : public BaseFormComponent
+	{
+	public:
+		TES_RTTI_VTABLE(BGSForcedLocRefType);
+
+		// members
+		std::uint64_t unk08;  // 08
+		std::uint64_t unk10;  // 10
+	};
+	static_assert(sizeof(BGSForcedLocRefType) == 0x18);
+}
+
---
+++ CommonLibSF/src/RE/B/BGSForcedLocRefType.cpp
@@ -0,0 +1,2 @@
+#include "RE/B/BGSForcedLocRefType.h"
+
