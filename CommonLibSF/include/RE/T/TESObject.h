213450eee21e9c7f337f68007893d1fd7a3f6b11
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:30:33 +0800

    fix: `TESObject`

--- CommonLibSF/include/RE/T/TESObject.h
+++ CommonLibSF/include/RE/T/TESObject.h
@@ -9,6 +9,18 @@ namespace RE
 	{
 	public:
 		TES_RTTI_VTABLE(TESObject);
+
+		// add
+		virtual void Unk_62();  // 62
+		virtual void Unk_63();  // 63
+		virtual void Unk_64();  // 64
+		virtual void Unk_65();  // 65
+		virtual void Unk_66();  // 66
+		virtual void Unk_67();  // 67
+		virtual void Unk_68();  // 68
+		virtual void Unk_69();  // 69
+		virtual void Unk_6A();  // 6A
+		virtual void Unk_6B();  // 6B
 	};
 }
 
