77db0037b989c32f9aa0d159082c3e19580ebd1c
vcpkg-action <gottyduke@hotmail.com>
2023-09-11 19:06:58 +0800

    feat: `BGSAttackDataForm`

---
+++ CommonLibSF/include/RE/B/BGSAttackDataForm.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "RE/B/BaseFormComponent.h"
+
+namespace RE
+{
+	class BGSAttackDataForm : public BaseFormComponent
+	{
+	public:
+		TES_RTTI_VTABLE(BGSAttackDataForm);
+
+		// members
+		void* /*NiPointer<BGSAttackDataMap>*/ attackDataMap;  // 08
+	};
+	static_assert(sizeof(BGSAttackDataForm) == 0x10);
+}
+
