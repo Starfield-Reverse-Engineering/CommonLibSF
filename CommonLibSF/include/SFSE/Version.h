8e8aa62852d3dfc5238ead819859db0501832e3c
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2023-09-13 13:41:19 -0700

    chore: bump Starfield and SFSE versions (#42)

--- CommonLibSF/include/SFSE/Version.h
+++ CommonLibSF/include/SFSE/Version.h
@@ -4,7 +4,8 @@ namespace SFSE
 {
 	constexpr REL::Version RUNTIME_SF_1_6_35(1, 6, 35, 0);
 	constexpr REL::Version RUNTIME_SF_1_7_23(1, 7, 23, 0);
-	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_7_23;
+	constexpr REL::Version RUNTIME_SF_1_7_29(1, 7, 29, 0);
+	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_7_29;
 
-	constexpr REL::Version SFSE_PACK_LATEST(0, 1, 0, 0);
+	constexpr REL::Version SFSE_PACK_LATEST(0, 1, 1, 0);
 }  // namespace SFSE
