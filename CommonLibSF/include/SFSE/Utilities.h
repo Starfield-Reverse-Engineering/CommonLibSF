79f7a8c221238f77f5f636e8ae859890283ad9ae
Angad <angadmisra28@gmail.com>
2023-09-13 19:01:56 -0700

    fix: appeasement

--- CommonLibSF/include/SFSE/Utilities.h
+++ CommonLibSF/include/SFSE/Utilities.h
@@ -11,28 +11,28 @@ namespace SFSE
 		constexpr void write_thunk_call() noexcept
 		{
 			SFSE::AllocTrampoline(14);
-			auto& trampoline{ SFSE::GetTrampoline() };
+			auto& trampoline = SFSE::GetTrampoline();
 			T::func = trampoline.write_call<Size>(T::address, T::Thunk);
 		}
 
 		template <class TDest, class TSource>
 		constexpr void write_vfunc() noexcept
 		{
-			REL::Relocation vtbl{ TDest::VTABLE[0] };
+			REL::Relocation<std::uintptr_t> vtbl{ TDest::VTABLE[0] };
 			TSource::func = vtbl.write_vfunc(TSource::idx, TSource::Thunk);
 		}
 
 		template <class TDest, class TSource>
 		constexpr void write_vfunc(const std::size_t a_vtable_idx) noexcept
 		{
-			REL::Relocation vtbl{ TDest::VTABLE[a_vtable_idx] };
+			REL::Relocation<std::uintptr_t> vtbl{ TDest::VTABLE[a_vtable_idx] };
 			TSource::func = vtbl.write_vfunc(TSource::idx, TSource::Thunk);
 		}
 
 		template <class T>
 		constexpr void write_vfunc(const REL::Offset a_offset) noexcept
 		{
-			REL::Relocation vtbl{ a_offset };
+			REL::Relocation<std::uintptr_t> vtbl{ a_offset };
 			T::func = vtbl.write_vfunc(T::idx, T::Thunk);
 		}
 	} // namespace stl
