6799ef9696db8e9949a5613069d3958b38f62b3c
shadeMe <shademehere@gmail.com>
2023-09-18 20:42:07 +0200

    Formatting

--- CommonLibSF/include/RE/T/TESTopicInfo.h
+++ CommonLibSF/include/RE/T/TESTopicInfo.h
@@ -14,18 +14,17 @@ namespace RE
 		SF_RTTI_VTABLE(TESTopicInfo);
 		SF_FORMTYPE(INFO);
 
-		// members
-		/*38*/ TESTopicInfo* unk38;
-		/*40*/ std::uint64_t unk40;
-		/*48*/ std::uint64_t unk48;
-		/*50*/ TESTopicInfo* unk50;  // Left/right (in the info list)
-		/*58*/ std::uint8_t  unk58;
-		/*59*/ std::uint8_t  pad59[3];
-		/*5C*/ std::uint32_t unk5C;
-		/*60*/ std::uint64_t unk60;
-		/*68*/ std::uint32_t unk68;  // Initialized to 0x1FFFFi64, pointer handle?
-		/*6C*/ std::uint32_t unk6C;  // Flags of some kind
-		/*70*/ TESResponse*  responses;
+		TESTopicInfo* unk38;      // 38
+		std::uint64_t unk40;      // 40
+		std::uint64_t unk48;      // 48
+		TESTopicInfo* unk50;      // 50 - Left/right (in the info list)
+		std::uint8_t  unk58;      // 58
+		std::uint8_t  pad59[3];   // 59
+		std::uint32_t unk5C;      // 5C
+		std::uint64_t unk60;      // 60
+		std::uint32_t unk68;      // 68 - BSPointerHandleRef?
+		std::uint32_t unk6C;      // 6C - Flags of some kind
+		TESResponse*  responses;  // 70
 	};
 	static_assert(sizeof(TESTopicInfo) == 0x78);
 }
