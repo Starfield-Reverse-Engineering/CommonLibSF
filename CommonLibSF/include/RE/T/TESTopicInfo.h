2e623e9df513a202efefcf380cd5b01b77788fd1
Madeesh Kannan <shadeMe@users.noreply.github.com>
2023-09-18 21:05:27 +0200

    feat: add class `TESTopicInfo` (#68)

--- CommonLibSF/include/RE/T/TESTopicInfo.h
+++ CommonLibSF/include/RE/T/TESTopicInfo.h
@@ -4,13 +4,27 @@
 
 namespace RE
 {
-	class TESTopicInfo : public TESForm
+	class TESResponse;
+
+	// 78
+	class TESTopicInfo :
+		public TESForm
 	{
 	public:
 		SF_RTTI_VTABLE(TESTopicInfo);
 		SF_FORMTYPE(INFO);
 
-		// members
-		std::uint64_t unk38;  // 38
+		TESTopicInfo* unk38;      // 38
+		std::uint64_t unk40;      // 40
+		std::uint64_t unk48;      // 48
+		TESTopicInfo* unk50;      // 50 - Left/right (in the info list)
+		std::uint8_t  unk58;      // 58
+		std::uint8_t  pad59[3];   // 59
+		std::uint32_t unk5C;      // 5C
+		std::uint64_t unk60;      // 60
+		std::uint32_t unk68;      // 68 - BSPointerHandleRef?
+		std::uint32_t unk6C;      // 6C - Flags of some kind
+		TESResponse*  responses;  // 70
 	};
+	static_assert(sizeof(TESTopicInfo) == 0x78);
 }
