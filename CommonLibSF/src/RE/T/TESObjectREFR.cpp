3fca31cc41e91dcb7922e09165b9a2c44b105068
TommInfinite <81481291+TommInfinite@users.noreply.github.com>
2023-09-18 19:09:54 +0300

    Update TESObjectREFR.cpp

--- CommonLibSF/src/RE/T/TESObjectREFR.cpp
+++ CommonLibSF/src/RE/T/TESObjectREFR.cpp
@@ -9,15 +9,6 @@ namespace RE
 		return func(this);
 	}
 
-	bool GetItemCount_Int(TESObjectREFR** TargetREF, TESForm* MyForm, std::uint64_t pad, float* result);
-
-	bool TESObjectREFR::GetItemCount(TESForm* a_Form, std::uint64_t a_arg2, float* a_result)
-	{
-		using func_t = decltype(&GetItemCount_Int);
-		REL::Relocation<func_t> func{ REL::Offset(0x01A7A6DC) };
-		TESObjectREFR*          MyRef = this;
-		return func((&MyRef), a_Form, a_arg2, a_result);
-	}
 
 	bool TESObjectREFR::HasKeyword(BGSKeyword* a_keyword)
 	{
