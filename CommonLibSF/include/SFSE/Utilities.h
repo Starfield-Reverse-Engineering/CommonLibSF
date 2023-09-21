d5afb104e570b5581cef4d96d57fb10f316c01d0
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-09-21 21:21:11 +0530

    feat: add `write_thunk_call` variant, simplify code

--- CommonLibSF/include/SFSE/Utilities.h
+++ CommonLibSF/include/SFSE/Utilities.h
@@ -8,25 +8,17 @@ namespace SFSE
 	namespace stl
 	{
 		template <class T, std::size_t Size = 5>
-		constexpr void write_thunk_call() noexcept
+		constexpr void write_thunk_call(const std::uintptr_t a_address) noexcept
 		{
 			SFSE::AllocTrampoline(14);
 			auto& trampoline = SFSE::GetTrampoline();
-			T::func = trampoline.write_call<Size>(T::address, T::Thunk);
+			T::func = trampoline.write_call<Size>(a_address, T::Thunk);
 		}
-
-		template <class To, class From>
-		constexpr void write_vfunc() noexcept
+		
+		template <class T, std::size_t Size = 5>
+		constexpr void write_thunk_call() noexcept
 		{
-			REL::Relocation<std::uintptr_t> vtbl{ To::VTABLE[0] };
-			From::func = vtbl.write_vfunc(From::idx, From::Thunk);
-		}
-
-		template <class To, class From>
-		constexpr void write_vfunc(const std::size_t a_vtable_idx) noexcept
-		{
-			REL::Relocation<std::uintptr_t> vtbl{ To::VTABLE[a_vtable_idx] };
-			From::func = vtbl.write_vfunc(From::idx, From::Thunk);
+			write_thunk_call<T, Size>(T::address);
 		}
 
 		template <class T>
@@ -34,6 +26,18 @@ namespace SFSE
 		{
 			REL::Relocation<std::uintptr_t> vtbl{ a_offset };
 			T::func = vtbl.write_vfunc(T::idx, T::Thunk);
+		}
+
+		template <class To, class From>
+		constexpr void write_vfunc(const std::size_t a_vtable_idx) noexcept
+		{
+			write_vfunc<From>(To::VTABLE[a_vtable_idx]);
+		}
+
+		template <class To, class From>
+		constexpr void write_vfunc() noexcept
+		{
+			write_vfunc<From>(To::VTABLE[0]);
 		}
 	}  // namespace stl
 }  // namespace SFSE
