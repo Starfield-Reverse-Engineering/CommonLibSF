3ea78c084620b21417a2cc4d18769cb866e8fb97
TommInfinite <81481291+TommInfinite@users.noreply.github.com>
2023-09-24 01:24:43 +0300

    Update BGSLocation.h

--- CommonLibSF/include/RE/B/BGSLocation.h
+++ CommonLibSF/include/RE/B/BGSLocation.h
@@ -5,8 +5,8 @@
 namespace RE
 {
 	class BGSLocation :
-		public TESForm  // 00
-						// ...
+		public TESForm,  // 00
+        public TESFullName // 20// ...
 	{
 	public:
 		SF_RTTI_VTABLE(BGSLocation);
