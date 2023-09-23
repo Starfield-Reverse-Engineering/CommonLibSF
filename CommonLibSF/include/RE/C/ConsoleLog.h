753955451b34c5194fb3c34fcd7b154405b03213
Qudix <17361645+Qudix@users.noreply.github.com>
2023-09-23 17:45:06 -0500

    fix!: consolelog print/vprint

--- CommonLibSF/include/RE/C/ConsoleLog.h
+++ CommonLibSF/include/RE/C/ConsoleLog.h
@@ -14,9 +14,17 @@ namespace RE
 			return *singleton;
 		}
 
-		void Print(const char* a_fmt, std::va_list a_args)
+		void Print(const char* a_fmt, ...)
 		{
-			using func_t = decltype(&ConsoleLog::Print);
+			std::va_list args;
+			va_start(args, a_fmt);
+			VPrint(a_fmt, args);
+			va_end(args);
+		}
+
+		void VPrint(const char* a_fmt, std::va_list a_args)
+		{
+			using func_t = decltype(&ConsoleLog::VPrint);
 			REL::Relocation<func_t> func{ REL::Offset(0x02883978) };
 			func(this, a_fmt, a_args);
 		}
