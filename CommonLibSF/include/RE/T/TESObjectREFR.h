8709834961d188995db7d73e7b3f0b1445a322b9
TommInfinite <81481291+TommInfinite@users.noreply.github.com>
2023-09-24 04:17:37 +0300

    Update TESObjectREFR.h

--- CommonLibSF/include/RE/T/TESObjectREFR.h
+++ CommonLibSF/include/RE/T/TESObjectREFR.h
@@ -294,7 +294,7 @@ namespace RE
 		[[nodiscard]] constexpr float       GetPositionX() const noexcept { return data.location.x; }
 		[[nodiscard]] constexpr float       GetPositionY() const noexcept { return data.location.y; }
 		[[nodiscard]] constexpr float       GetPositionZ() const noexcept { return data.location.z; }
-		[[nodiscard]] Actor* GetSpaceshipPilot();
+		[[nodiscard]] Actor* 				GetSpaceshipPilot();
 		[[nodiscard]] std::int32_t          GetValue();
 		[[nodiscard]] bool                  HasKeyword(BGSKeyword* a_keyword);
 		[[nodiscard]] bool                  IsCrimeToActivate();
