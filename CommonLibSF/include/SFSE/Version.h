eab59f3b12ffe4542e3f48896e799dc3b9efc9e4
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2023-09-25 18:58:23 -0600

    Bump Game Version and SFSE Version for 1.7.33 (#95)

--- CommonLibSF/include/SFSE/Version.h
+++ CommonLibSF/include/SFSE/Version.h
@@ -5,7 +5,8 @@ namespace SFSE
 	constexpr REL::Version RUNTIME_SF_1_6_35(1, 6, 35, 0);
 	constexpr REL::Version RUNTIME_SF_1_7_23(1, 7, 23, 0);
 	constexpr REL::Version RUNTIME_SF_1_7_29(1, 7, 29, 0);
-	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_7_29;
+	constexpr REL::Version RUNTIME_SF_1_7_33(1, 7, 33, 0);
+	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_7_33;
 
-	constexpr REL::Version SFSE_PACK_LATEST(0, 1, 2, 0);
+	constexpr REL::Version SFSE_PACK_LATEST(0, 1, 3, 0);
 }  // namespace SFSE
