140af8b71f8e72ef83cce765962d31a160931e4e
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-09-27 01:03:49 +0530

    feat: add `BSTEventSource` event registration, `BSTGlobalEvent` (#103)

--- CommonLibSF/include/RE/B/BSTEvent.h
+++ CommonLibSF/include/RE/B/BSTEvent.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "BSTSingleton.h"
+
 namespace RE
 {
 	enum class BSEventNotifyControl : std::uint32_t
@@ -22,6 +24,9 @@ namespace RE
 		public:
 			virtual ~SourceBase() = 0;
 		};
+
+		inline SinkBase::~SinkBase() = default;
+		inline SourceBase::~SourceBase() = default;
 	}
 
 	template <class>
@@ -36,11 +41,49 @@ namespace RE
 		// add
 		virtual BSEventNotifyControl ProcessEvent(const Event& a_event, BSTEventSource<Event>* a_source) = 0;  // 01
 	};
+	static_assert(sizeof(BSTEventSink<void*>) == 0x8);
 
 	template <class Event>
 	class BSTEventSource : public BSTEventDetail::SourceBase
 	{
 	public:
 		~BSTEventSource() override = default;  // 00
+
+		void Notify(const Event& a_event)
+		{
+			using func_t = decltype(&BSTEventSource::Notify);
+			REL::Relocation<func_t> func{ REL::ID(178573) };
+			return func(this, a_event);
+		}
+		void RegisterSink(BSTEventSink<Event>* a_sink)
+		{
+			using func_t = decltype(&BSTEventSource::RegisterSink);
+			REL::Relocation<func_t> func{ REL::ID(34452) };
+			return func(this, a_sink);
+		}
+		void UnregisterSink(BSTEventSink<Event>* a_sink)
+		{
+			using func_t = decltype(&BSTEventSource::UnregisterSink);
+			REL::Relocation<func_t> func{ REL::ID(34451) };
+			return func(this, a_sink);
+		}
 	};
+
+	class BSTGlobalEvent
+	{
+	public:
+		template <class Event>
+		class EventSource :
+			public BSTSingletonImplicit<EventSource<Event>>,
+			public BSTEventSource<Event>
+		{
+		public:
+			~EventSource() override;  // 00
+
+			virtual BSEventNotifyControl ProcessEvent(const Event& a_event, BSTEventSource<Event>* a_source);  // 01
+		};
+
+		virtual ~BSTGlobalEvent();  // 00
+	};
+	static_assert(sizeof(BSTGlobalEvent) == 0x08);
 }
