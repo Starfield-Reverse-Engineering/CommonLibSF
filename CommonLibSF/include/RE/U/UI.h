388a77f8cda39bf628d95306e640aca953072e78
Qudix <17361645+Qudix@users.noreply.github.com>
2023-09-27 13:41:24 -0500

    fix: wrong rel id

--- CommonLibSF/include/RE/U/UI.h
+++ CommonLibSF/include/RE/U/UI.h
@@ -12,9 +12,8 @@ namespace RE
 		bool IsMenuOpen(const BSFixedString& a_name)
 		{
 			using func_t = decltype(&UI::IsMenuOpen);
-			REL::Relocation<func_t> func{ REL::ID(187048) };
+			REL::Relocation<func_t> func{ REL::ID(187049) };
 			return func(this, a_name);
 		}
 	};
-
 }
