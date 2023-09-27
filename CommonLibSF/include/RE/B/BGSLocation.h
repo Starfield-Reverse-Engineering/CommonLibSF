09b675c8d3d1832c4e1e51ac7ca1460086389b07
maintenance <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-27 23:58:34 +0000

    ci: maintenance `2023-09-27.2`

--- CommonLibSF/include/RE/B/BGSLocation.h
+++ CommonLibSF/include/RE/B/BGSLocation.h
@@ -1,33 +1,33 @@
 #pragma once
 
+#include "RE/A/ActorValueOwner.h"
+#include "RE/B/BGSKeywordForm.h"
+#include "RE/B/BGSPropertySheet.h"
 #include "RE/T/TESForm.h"
 #include "RE/T/TESFullName.h"
-#include "RE/A/ActorValueOwner.h"
-#include "RE/B/BGSPropertySheet.h"
-#include "RE/B/BGSKeywordForm.h"
 
 namespace RE
 {
 	class BGSLocation :
-		public TESForm,         // 00
-		public TESFullName,     // 38
-        public BGSKeywordForm,  // 48
-        public BGSPropertySheet,// 78
-        public ActorValueOwner  // 88
+		public TESForm,           // 00
+		public TESFullName,       // 38
+		public BGSKeywordForm,    // 48
+		public BGSPropertySheet,  // 78
+		public ActorValueOwner    // 88
 	{
 	public:
 		SF_RTTI_VTABLE(BGSLocation);
 		SF_FORMTYPE(LCTN);
 
-        std::int8_t  pad90[25];             // 90
-        std::uint8_t minLevel;              // A9
-        std::int8_t  padAA;                 // AA
-        std::uint8_t maxLevel;              // AB
-        std::int8_t  padAC[20];             // AC
-        BGSLocation* parentLocation;        // C0
-        std::int8_t  padC8[276];            // C8
-        bool         isExplored;           	// 1DC
-        bool         hasBeenEverExplored;  	// 1DD
-        //
+		std::int8_t  pad90[25];            // 90
+		std::uint8_t minLevel;             // A9
+		std::int8_t  padAA;                // AA
+		std::uint8_t maxLevel;             // AB
+		std::int8_t  padAC[20];            // AC
+		BGSLocation* parentLocation;       // C0
+		std::int8_t  padC8[276];           // C8
+		bool         isExplored;           // 1DC
+		bool         hasBeenEverExplored;  // 1DD
+										   //
 	};
 }
