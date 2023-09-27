ce11d5ddb5ba96f8c6b4325c4a70cd8f54b14cfb
TommInfinite <81481291+TommInfinite@users.noreply.github.com>
2023-09-28 02:57:49 +0300

    feat: RE BGS Location (#111)

--- CommonLibSF/include/RE/B/BGSLocation.h
+++ CommonLibSF/include/RE/B/BGSLocation.h
@@ -2,16 +2,32 @@
 
 #include "RE/T/TESForm.h"
 #include "RE/T/TESFullName.h"
+#include "RE/A/ActorValueOwner.h"
+#include "RE/B/BGSPropertySheet.h"
+#include "RE/B/BGSKeywordForm.h"
 
 namespace RE
 {
 	class BGSLocation :
-		public TESForm,     // 00
-		public TESFullName  // 20
-							// ...
+		public TESForm,         // 00
+		public TESFullName,     // 38
+        public BGSKeywordForm,  // 48
+        public BGSPropertySheet,// 78
+        public ActorValueOwner  // 88
 	{
 	public:
 		SF_RTTI_VTABLE(BGSLocation);
 		SF_FORMTYPE(LCTN);
+
+        std::int8_t  pad90[25];             // 90
+        std::uint8_t minLevel;              // A9
+        std::int8_t  padAA;                 // AA
+        std::uint8_t maxLevel;              // AB
+        std::int8_t  padAC[20];             // AC
+        BGSLocation* parentLocation;        // C0
+        std::int8_t  padC8[276];            // C8
+        bool         isExplored;           	// 1DC
+        bool         hasBeenEverExplored;  	// 1DD
+        //
 	};
 }
