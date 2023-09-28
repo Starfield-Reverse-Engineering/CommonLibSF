1974e54e4b217be3b4a673c5f4e23215f336fb99
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-09-28 20:52:28 +0530

    chore: remove comments

--- CommonLibSF/include/RE/N/NiSmartPointer.h
+++ CommonLibSF/include/RE/N/NiSmartPointer.h
@@ -8,13 +8,10 @@ namespace RE
 	public:
 		using element_type = T;
 
-		// 1)
 		constexpr NiPointer() noexcept = default;
 
-		// 2)
 		constexpr NiPointer(std::nullptr_t) noexcept {}
 
-		// 3)
 		template <class Y>
 		explicit NiPointer(Y* a_rhs)  //
 			requires(std::convertible_to<Y*, element_type*>)
@@ -24,14 +21,12 @@ namespace RE
 			TryAttach();
 		}
 
-		// 9a)
 		NiPointer(const NiPointer& a_rhs) :
 			_ptr(a_rhs._ptr)
 		{
 			TryAttach();
 		}
 
-		// 9b)
 		template <class Y>
 		NiPointer(const NiPointer<Y>& a_rhs)  //
 			requires(std::convertible_to<Y*, element_type*>)
@@ -41,12 +36,10 @@ namespace RE
 			TryAttach();
 		}
 
-		// 10a)
 		NiPointer(NiPointer&& a_rhs) noexcept :
 			_ptr(std::exchange(a_rhs._ptr, nullptr))
 		{}
 
-		// 10b)
 		template <class Y>
 		NiPointer(NiPointer<Y>&& a_rhs) noexcept  //
 			requires(std::convertible_to<Y*, element_type*>)
@@ -56,7 +49,6 @@ namespace RE
 
 		~NiPointer() noexcept { reset(); }
 
-		// 1a)
 		NiPointer& operator=(const NiPointer& a_rhs)
 		{
 			if (this != std::addressof(a_rhs)) {
@@ -67,7 +59,6 @@ namespace RE
 			return *this;
 		}
 
-		// 1b)
 		template <class Y>
 		NiPointer& operator=(const NiPointer<Y>& a_rhs)  //
 			requires(std::convertible_to<Y*, element_type*>)
@@ -78,7 +69,6 @@ namespace RE
 			return *this;
 		}
 
-		// 2a)
 		NiPointer& operator=(NiPointer&& a_rhs) noexcept
 		{
 			if (this != std::addressof(a_rhs)) {
@@ -88,7 +78,6 @@ namespace RE
 			return *this;
 		}
 
-		// 2b)
 		template <class Y>
 		NiPointer& operator=(NiPointer<Y>&& a_rhs) noexcept  //
 			requires(std::convertible_to<Y*, element_type*>)
@@ -98,10 +87,8 @@ namespace RE
 			return *this;
 		}
 
-		// 1)
 		void reset() { TryDetach(); }
 
-		// 2)
 		template <class Y>
 		void reset(Y* a_ptr)  //
 			requires(std::convertible_to<Y*, element_type*>)
@@ -159,28 +146,24 @@ namespace RE
 		return NiPointer<T>{ new T(std::forward<Args>(a_args)...) };
 	}
 
-	// 1)
 	template <class T, class U>
 	[[nodiscard]] constexpr bool operator==(const NiPointer<T>& a_lhs, const NiPointer<U>& a_rhs) noexcept
 	{
 		return a_lhs.get() == a_rhs.get();
 	}
 
-	// 7)
 	template <class T, class U>
 	[[nodiscard]] constexpr std::strong_ordering operator<=>(const NiPointer<T>& a_lhs, const NiPointer<U>& a_rhs) noexcept
 	{
 		return std::compare_three_way{}(a_lhs.get(), a_rhs.get());
 	}
 
-	// 8)
 	template <class T>
 	[[nodiscard]] constexpr bool operator==(const NiPointer<T>& a_lhs, std::nullptr_t) noexcept
 	{
 		return !a_lhs;
 	}
 
-	// 20)
 	template <class T>
 	[[nodiscard]] constexpr std::strong_ordering operator<=>(const NiPointer<T>& a_lhs, std::nullptr_t) noexcept
 	{
