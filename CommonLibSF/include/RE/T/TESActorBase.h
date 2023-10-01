fa3f4699ecca2540a3aa6ba11e9171f1e3ce00c4
TommInfinite <81481291+TommInfinite@users.noreply.github.com>
2023-10-02 02:52:58 +0300

    Update TESActorBase.h

--- CommonLibSF/include/RE/T/TESActorBase.h
+++ CommonLibSF/include/RE/T/TESActorBase.h
@@ -7,8 +7,8 @@
 #include "RE/B/BGSPerkRankArray.h"
 #include "RE/B/BGSPropertySheet.h"
 #include "RE/B/BGSSkinForm.h"
-#include "RE/T/TESAIForm.h"
 #include "RE/T/TESActorBaseData.h"
+#include "RE/T/TESAIForm.h"
 #include "RE/T/TESBoundAnimObject.h"
 #include "RE/T/TESContainer.h"
 #include "RE/T/TESFullName.h"
@@ -16,58 +16,41 @@
 
 namespace RE
 {
-	class TESActorBase :
-		public TESBoundAnimObject,
-		public TESActorBaseData,           // 118
-		public TESContainer,               // 188
-		public TESSpellList,               // 1A0
-		public TESAIForm,                  // 1B8
-		public TESFullName,                // 1F8
-		public ActorValueOwner,            // 208
-		public BGSDestructibleObjectForm,  // 210
-		public BGSSkinForm,                // 220
-		public BGSKeywordForm,             // 230
-		public BGSAttackDataForm,          // 260
-		public BGSPerkRankArray,           // 270
-		public BGSPropertySheet            // 288
-	{
-	public:
-		SF_RTTI_VTABLE(TESActorBase);
+    class TESActorBase : public TESBoundAnimObject,
+                         public TESActorBaseData,          // 118
+                         public TESContainer,              // 188
+                         public TESSpellList,              // 1A0
+                         public TESAIForm,                 // 1B8
+                         public TESFullName,               // 1F8
+                         public ActorValueOwner,           // 208
+                         public BGSDestructibleObjectForm, // 210
+                         public BGSSkinForm,               // 220
+                         public BGSKeywordForm,            // 230
+                         public BGSAttackDataForm,         // 260
+                         public BGSPerkRankArray,          // 270
+                         public BGSPropertySheet           // 288
+    {
+    public:
+        SF_RTTI_VTABLE(TESActorBase);
 
-		// add
-		virtual void Unk_82();  // 82
-		virtual void Unk_83();  // 83
-		virtual void Unk_84();  // 84
-		virtual void Unk_85();  // 85
+        enum class Pronoun_Type
+        {
+            kNotSet,
+            kHeHim,
+            kSheHer,
+            kTheyThem
+        };
 
-		// members
-		/**
-		TESActorBaseData          actorBaseData;       // 118
-		TESContainer              container;           // 188
-		TESSpellList              spellList;           // 1A0
-		TESAIForm                 aiForm;              // 1B8
-		TESFullName               fullName;            // 1F8
-		ActorValueOwner           actorValue;          // 208
-		BGSDestructibleObjectForm destructibleObject;  // 210
-		BGSSkinForm               skinForm;            // 220
-		BGSKeywordForm            keywords;            // 230
-		BGSAttackDataForm         attackData;          // 260
-		BGSPerkRankArray          perkRankArray;       // 270
-		BGSPropertySheet          propertySheet;       // 288
-		*/
-	};
-	/*
-	static_assert(offsetof(TESActorBase, actorBaseData) == 0x118);
-	static_assert(offsetof(TESActorBase, container) == 0x188);
-	static_assert(offsetof(TESActorBase, spellList) == 0x1A0);
-	static_assert(offsetof(TESActorBase, aiForm) == 0x1B8);
-	static_assert(offsetof(TESActorBase, fullName) == 0x1F8);
-	static_assert(offsetof(TESActorBase, actorValue) == 0x208);
-	static_assert(offsetof(TESActorBase, destructibleObject) == 0x210);
-	static_assert(offsetof(TESActorBase, skinForm) == 0x220);
-	static_assert(offsetof(TESActorBase, keywords) == 0x230);
-	static_assert(offsetof(TESActorBase, attackData) == 0x260);
-	static_assert(offsetof(TESActorBase, perkRankArray) == 0x270);
-	static_assert(offsetof(TESActorBase, propertySheet) == 0x288);
-	*/
-}
+        // add
+        virtual void Unk_82(); // 82
+        virtual void Unk_83(); // 83
+        virtual void Unk_84(); // 84
+        virtual void Unk_85(); // 85
+
+        // members
+        std::uint8_t pad298[488];                               // 298
+        stl::enumeration<Pronoun_Type, std::uint8_t> pronoun;     // 480
+        //
+    };
+
+} // namespace RE
