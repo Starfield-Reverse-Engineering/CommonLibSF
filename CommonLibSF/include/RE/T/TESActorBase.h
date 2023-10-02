6c985863ddb98e546b0d2106476f64ba5cfc80db
maintenance <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 17:56:41 +0000

    ci: maintenance `2023-10-02.3`

--- CommonLibSF/include/RE/T/TESActorBase.h
+++ CommonLibSF/include/RE/T/TESActorBase.h
@@ -7,8 +7,8 @@
 #include "RE/B/BGSPerkRankArray.h"
 #include "RE/B/BGSPropertySheet.h"
 #include "RE/B/BGSSkinForm.h"
-#include "RE/T/TESActorBaseData.h"
 #include "RE/T/TESAIForm.h"
+#include "RE/T/TESActorBaseData.h"
 #include "RE/T/TESBoundAnimObject.h"
 #include "RE/T/TESContainer.h"
 #include "RE/T/TESFullName.h"
@@ -16,42 +16,42 @@
 
 namespace RE
 {
-    class TESActorBase :
-        public TESBoundAnimObject,
-        public TESActorBaseData,          // 118
-        public TESContainer,              // 188
-        public TESSpellList,              // 1A0
-        public TESAIForm,                 // 1B8
-        public TESFullName,               // 1F8
-        public ActorValueOwner,           // 208
-        public BGSDestructibleObjectForm, // 210
-        public BGSSkinForm,               // 220
-        public BGSKeywordForm,            // 230
-        public BGSAttackDataForm,         // 260
-        public BGSPerkRankArray,          // 270
-        public BGSPropertySheet           // 288
-    {
-    public:
-        SF_RTTI_VTABLE(TESActorBase);
+	class TESActorBase :
+		public TESBoundAnimObject,
+		public TESActorBaseData,           // 118
+		public TESContainer,               // 188
+		public TESSpellList,               // 1A0
+		public TESAIForm,                  // 1B8
+		public TESFullName,                // 1F8
+		public ActorValueOwner,            // 208
+		public BGSDestructibleObjectForm,  // 210
+		public BGSSkinForm,                // 220
+		public BGSKeywordForm,             // 230
+		public BGSAttackDataForm,          // 260
+		public BGSPerkRankArray,           // 270
+		public BGSPropertySheet            // 288
+	{
+	public:
+		SF_RTTI_VTABLE(TESActorBase);
 
-        enum class Pronoun_Type
-        {
-            kNotSet,
-            kHeHim,
-            kSheHer,
-            kTheyThem
-        };
+		enum class Pronoun_Type
+		{
+			kNotSet,
+			kHeHim,
+			kSheHer,
+			kTheyThem
+		};
 
-        // add
-        virtual void Unk_82(); // 82
-        virtual void Unk_83(); // 83
-        virtual void Unk_84(); // 84
-        virtual void Unk_85(); // 85
+		// add
+		virtual void Unk_82();  // 82
+		virtual void Unk_83();  // 83
+		virtual void Unk_84();  // 84
+		virtual void Unk_85();  // 85
 
-        // members
-        std::uint8_t pad298[488];                                   // 298
-        stl::enumeration<Pronoun_Type, std::uint8_t> pronoun;       // 480
-        //
-    };
+		// members
+		std::uint8_t                                 pad298[488];  // 298
+		stl::enumeration<Pronoun_Type, std::uint8_t> pronoun;      // 480
+		//
+	};
 
-} // namespace RE
+}  // namespace RE
