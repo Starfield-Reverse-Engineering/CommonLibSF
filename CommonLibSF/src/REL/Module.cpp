0e0ee582046a1d128cf5787ce7f22b07f8b95cd6
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2023-10-02 09:09:06 -0700

    fix: use std::ranges::find_if in Module.cpp

--- CommonLibSF/src/REL/Module.cpp
+++ CommonLibSF/src/REL/Module.cpp
@@ -15,7 +15,7 @@ namespace REL
 
 		for (std::size_t i = 0; i < size; ++i) {
 			const auto& section = sections[i];
-			const auto  it = std::find_if(SEGMENTS.begin(), SEGMENTS.end(), [&](auto&& a_elem) {
+			const auto  it = std::ranges::find_if(SEGMENTS.begin(), SEGMENTS.end(), [&](auto&& a_elem) {
                 constexpr auto size = std::extent_v<decltype(section.name)>;
                 const auto     len = (std::min)(a_elem.first.size(), size);
                 return std::memcmp(a_elem.first.data(), section.name, len) == 0 && (section.characteristics & a_elem.second) == a_elem.second;
