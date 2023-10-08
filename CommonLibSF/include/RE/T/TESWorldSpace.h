b79e3db5c0d9ceda41e50fb82537d7c4acbe9be0
TommInfinite <81481291+TommInfinite@users.noreply.github.com>
2023-10-08 15:11:31 +0300

    Update TESWorldSpace.h

--- CommonLibSF/include/RE/T/TESWorldSpace.h
+++ CommonLibSF/include/RE/T/TESWorldSpace.h
@@ -2,11 +2,11 @@
 
 #include "RE/T/TESForm.h"
 #include "RE/T/TESFullName.h"
-#include "RE/B/BGSLocation.h"
 #include "RE/N/NiPoint3.h"
 
 namespace RE
 {
+	class BGSLocation;
 	class TESWorldSpace :
 		public TESForm,     // 00
 		public TESFullName  // 20
