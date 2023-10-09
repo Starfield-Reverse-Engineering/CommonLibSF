a21d31c59dea77ed10891cf81248c97dd9b7a97a
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2023-10-09 10:13:29 -0700

    fix: latest version in Version.h

--- CommonLibSF/include/SFSE/Version.h
+++ CommonLibSF/include/SFSE/Version.h
@@ -6,8 +6,8 @@ namespace SFSE
 	constexpr REL::Version RUNTIME_SF_1_7_23(1, 7, 23, 0);
 	constexpr REL::Version RUNTIME_SF_1_7_29(1, 7, 29, 0);
 	constexpr REL::Version RUNTIME_SF_1_7_33(1, 7, 33, 0);
-	constexpr REL::Version RUNTIME_SF_1_7_39(1, 7, 39, 0);
-	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_7_39;
+	constexpr REL::Version RUNTIME_SF_1_7_36(1, 7, 36, 0);
+	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_7_36;
 
 	constexpr REL::Version SFSE_PACK_LATEST(0, 1, 3, 0);
 }
