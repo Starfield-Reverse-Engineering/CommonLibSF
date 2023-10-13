e767c452a788096b2ae14a4cb54546ee71260787
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-10-13 07:02:32 +0530

    feat: add `BSTSmartPointer` (#163)

--- CommonLibSF/include/RE/B/BSIntrusiveRefCounted.h
+++ CommonLibSF/include/RE/B/BSIntrusiveRefCounted.h
@@ -1,14 +1,38 @@
 #pragma once
 
+#include "RE/M/MemoryManager.h"
+
 namespace RE
 {
 	struct BSIntrusiveRefCounted
 	{
 	public:
 		SF_RTTI(BSIntrusiveRefCounted);
+		
+		template <class>
+		friend struct BSTSmartPointerIntrusiveRefCount;
 
-		volatile mutable std::uint32_t refCount{};  // 00
-		std::uint32_t                  unk04;       // 04
+		constexpr BSIntrusiveRefCounted() noexcept {}
+		
+		std::uint32_t IncRef() const
+		{
+			stl::atomic_ref myRefCount{ _refCount };
+			return ++myRefCount;
+		}
+
+		std::uint32_t DecRef() const
+		{
+			stl::atomic_ref myRefCount{ _refCount };
+			return --myRefCount;
+		}
+
+		[[nodiscard]] constexpr std::uint32_t QRefCount() const noexcept { return _refCount; }
+
+		SF_HEAP_REDEFINE_NEW();
+
+	protected:
+		// members
+		mutable volatile std::uint32_t _refCount{};  // 0
 	};
-	static_assert(sizeof(BSIntrusiveRefCounted) == 0x08);
+	static_assert(sizeof(BSIntrusiveRefCounted) == 0x04);
 }
