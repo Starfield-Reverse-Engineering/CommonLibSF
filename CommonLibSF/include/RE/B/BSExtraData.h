c1db74812d40ca7086f4d421e4be21c45fc0c08e
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-10-19 07:56:57 +0530

    feat!: add `ExtraDataList`, `BSExtraData`

---
+++ CommonLibSF/include/RE/B/BSExtraData.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "RE/E/ExtraDataTypes.h"
+
+namespace RE
+{
+	class BSExtraData
+	{
+	public:
+		SF_RTTI_VTABLE(BSExtraData);
+		SF_EXTRADATATYPE(None);
+
+		virtual ~BSExtraData();  // 00
+
+		// add
+		virtual void Unk_01();  // 01
+		virtual void Unk_02();  // 02
+		virtual void Unk_03();  // 03
+		virtual void Unk_04();  // 04
+		virtual void Unk_05();  // 05
+		virtual void Unk_06();  // 06
+
+		// members
+		BSExtraData*                                  next;   // 08
+		std::uint16_t                                 flags;  // 10
+		stl::enumeration<ExtraDataType, std::uint8_t> type;   // 12
+	};
+	static_assert(sizeof(BSExtraData) == 0x18);
+}
