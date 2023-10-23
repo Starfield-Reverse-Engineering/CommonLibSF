dbc2f62d29bab276b5a960621269cf3ed2f21a86
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2023-10-22 19:33:09 -0700

    Squashed commit of the following:
    commit 1857f7f9f1c22120f3dc6f69ef0b0c9eddaa34a8
    Author: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Date:   Mon Oct 23 02:17:36 2023 +0000
    
        ci: maintenance `2023-10-23`
    
    commit 36eec91b6bdac28e1be534b5cecf6f760a0cc0a1
    Author: Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
    Date:   Sun Oct 22 19:16:57 2023 -0700
    
        chore: make `Module` and `Version` header-only, add some newlines (#188)

--- CommonLibSF/include/SFSE/Logger.h
+++ CommonLibSF/include/SFSE/Logger.h
@@ -26,7 +26,8 @@ namespace SFSE::log
 	SFSE_MAKE_SOURCE_LOGGER(critical, critical);
 
 	[[nodiscard]] std::optional<std::filesystem::path> log_directory();
-	void                                               init();
+
+	void init();
 }
 
 #undef SFSE_MAKE_SOURCE_LOGGER
