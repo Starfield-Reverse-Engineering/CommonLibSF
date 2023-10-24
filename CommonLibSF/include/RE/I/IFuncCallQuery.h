32dfc33a1218633891ec27fac496cab0133942db
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-10-24 15:50:35 -0700

    feat: Implement IFuncCallQuery, RawFuncCallQuery, CodeTasklet (#194)

---
+++ CommonLibSF/include/RE/I/IFuncCallQuery.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "RE/B/BSFixedString.h"
+#include "RE/B/BSIntrusiveRefCounted.h"
+#include "RE/B/BSTSmartPointer.h"
+
+namespace RE
+{
+	namespace BSScript
+	{
+
+		class StackFrame;
+		class Variable;
+		class ObjectTypeInfo;
+		class Object;
+
+		class IFuncCallQuery : public BSIntrusiveRefCounted
+		{
+		public:
+			SF_RTTI_VTABLE(BSScript__Internal__IFuncCallQuery);
+
+			enum class CallType
+			{
+				kMember,
+				kStatic,
+				kGetter,
+				kSetter
+			};
+			virtual ~IFuncCallQuery();  // 00
+
+			// add
+			virtual bool GetFunctionCallInfo(
+				CallType&                           a_callType,
+				BSTSmartPointer<ObjectTypeInfo>&    a_objectTypeInfo,
+				BSFixedString&                      a_name,
+				Variable&                           a_self,
+				void* /* BSScrapArray<Variable>& */ a_args) const = 0;    // 01
+			virtual BSTSmartPointer<Object> GetSelfAsObject() const = 0;  // 02
+		};
+		static_assert(sizeof(IFuncCallQuery) == 0x10);
+
+	};
+}
