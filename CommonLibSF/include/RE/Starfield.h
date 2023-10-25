ec63dc967ff26bc6050f7655bac8296e0c70dcbe
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-10-24 19:38:16 -0700

    feat: add Stack and StackFrame (#196)

--- CommonLibSF/include/RE/Starfield.h
+++ CommonLibSF/include/RE/Starfield.h
@@ -144,6 +144,8 @@
 #include "RE/S/SettingCollectionList.h"
 #include "RE/S/SettingCollectionMap.h"
 #include "RE/S/Sexes.h"
+#include "RE/S/Stack.h"
+#include "RE/S/StackFrame.h"
 #include "RE/S/Struct.h"
 #include "RE/S/StructTypeInfo.h"
 #include "RE/T/TBO_InstanceData.h"
