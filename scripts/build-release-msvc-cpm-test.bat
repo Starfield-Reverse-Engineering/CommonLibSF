faac191421a54a77184ca2f4329e8b9d2a10ffc7
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2023-10-24 21:28:17 -0700

    fix: -test.bat files

--- scripts/build-release-msvc-cpm-test.bat
+++ scripts/build-release-msvc-cpm-test.bat
@@ -1,4 +1,4 @@
 echo off
 rd /s /q "build"
-cmake -B "%~dp0/../build" -S "%~dp0/../CommonLibSF" --preset=build-release-msvc-ninja-cpm -DSFSE_ENABLE_TESTS
+cmake -B "%~dp0/../build" -S "%~dp0/../CommonLibSF" --preset=build-release-msvc-ninja-cpm -DSFSE_BUILD_TESTS=true
 cmake --build "%~dp0/../build" --config Release
