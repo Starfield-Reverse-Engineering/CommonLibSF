3ce2b89ebc20b253eb25ad83b6ba8de7347a5ceb
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2023-11-21 11:55:35 -0700

    Bump version for Starfield 1.8.86 and SFSE 0.1.6

--- CommonLibSF/include/SFSE/Version.h
+++ CommonLibSF/include/SFSE/Version.h
@@ -7,7 +7,8 @@ namespace SFSE
 	constexpr REL::Version RUNTIME_SF_1_7_29(1, 7, 29, 0);
 	constexpr REL::Version RUNTIME_SF_1_7_33(1, 7, 33, 0);
 	constexpr REL::Version RUNTIME_SF_1_7_36(1, 7, 36, 0);
-	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_7_36;
+	constexpr REL::Version RUNTIME_SF_1_8_86(1, 8, 86, 0);
+	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_8_86;
 
-	constexpr REL::Version SFSE_PACK_LATEST(0, 1, 5, 0);
+	constexpr REL::Version SFSE_PACK_LATEST(0, 1, 6, 0);
 }
