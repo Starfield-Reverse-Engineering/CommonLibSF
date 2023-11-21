c58dac40daa0096ffd6ba888bce1115d12152a52
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-11-22 01:27:02 +0530

    feat: `TESLeveledList` and related classes (#228)

---
+++ CommonLibSF/include/RE/B/BGSLevGenericBaseForm.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "RE/B/BGSForcedLocRefType.h"
+#include "RE/B/BGSModelMaterialSwap.h"
+#include "RE/T/TESBoundAnimObject.h"
+#include "RE/T/TESLeveledList.h"
+
+namespace RE
+{
+	class BGSLevGenericBaseForm :
+		public TESBoundAnimObject,   // 000
+		public TESLeveledList,       // 118
+		public BGSModelMaterialSwap  // 178
+	{
+	public:
+		SF_RTTI_VTABLE(BGSLevGenericBaseForm);
+		SF_FORMTYPE(LVLB);
+
+		~BGSLevGenericBaseForm() override;  // 00
+	};
+	static_assert(sizeof(BGSLevGenericBaseForm) == 0x1A0);
+}
---
+++ CommonLibSF/include/RE/B/BGSLevSpaceCell.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "RE/T/TESForm.h"
+#include "RE/T/TESLeveledList.h"
+
+namespace RE
+{
+	class BGSLevSpaceCell :
+		public TESForm,        // 00
+		public TESLeveledList  // 38
+	{
+	public:
+		SF_RTTI_VTABLE(BGSLevSpaceCell);
+		SF_FORMTYPE(LVSC);
+
+		~BGSLevSpaceCell() override;  // 00
+	};
+	static_assert(sizeof(BGSLevSpaceCell) == 0x98);
+}
