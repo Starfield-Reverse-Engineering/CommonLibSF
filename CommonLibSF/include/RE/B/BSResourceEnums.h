21e99bb31850620a64a109c9e79a1c0c8f579c80
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-04-09 10:59:12 -0700

    feat: add BSResourceEnums

---
+++ CommonLibSF/include/RE/B/BSResourceEnums.h
@@ -0,0 +1,56 @@
+#pragma once
+
+namespace RE::BSResource
+{
+	enum class ErrorCode : std::uint32_t
+	{
+		kNone = 0,
+		kNotExist = 1,
+		kInvalidPath = 2,
+		kFileError = 3,
+		kInvalidType = 4,
+		kMemoryError = 5,
+		kBusy = 6,
+		kInvalidParam = 7,
+		kUnsupported = 8
+	};
+
+	enum class SeekMode
+	{
+		kSet = 0,
+		kCurrent = 1,
+		kEnd = 2
+	};
+
+	struct FileID
+	{
+	public:
+		[[nodiscard]] bool operator==(const FileID&) const noexcept = default;
+
+		// members
+		std::uint32_t file = 0;  // 0
+		std::uint32_t ext = 0;   // 4
+	};
+	static_assert(sizeof(FileID) == 0x8);
+
+	struct ID :
+		public FileID  // 0
+	{
+	public:
+		[[nodiscard]] bool operator==(const ID&) const noexcept = default;
+
+		// members
+		std::uint32_t dir = 0;  // 8
+	};
+	static_assert(sizeof(BSResource::ID) == 0xC);
+
+	struct Info
+	{
+	public:
+		// members
+		WinAPI::FILETIME modifyTime;  // 00
+		WinAPI::FILETIME createTime;  // 08
+		std::uint64_t    fileSize;    // 10
+	};
+	static_assert(sizeof(Info) == 0x18);
+}
