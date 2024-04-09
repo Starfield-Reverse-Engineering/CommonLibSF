456a9996834684eb798660c858a08e143e72ae1b
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-04-09 10:59:12 -0700

    fix: add BSResourceEnums to starfield.h

--- CommonLibSF/include/RE/Starfield.h
+++ CommonLibSF/include/RE/Starfield.h
@@ -188,6 +188,7 @@
 #include "RE/B/BSLock.h"
 #include "RE/B/BSLog.h"
 #include "RE/B/BSReflection.h"
+#include "RE/B/BSResourceEnums.h"
 #include "RE/B/BSStorage.h"
 #include "RE/B/BSStringPool.h"
 #include "RE/B/BSStringT.h"
