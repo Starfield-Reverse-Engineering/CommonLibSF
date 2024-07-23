149e2c282d15f5c01ffea23179279c687e11c8a8
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-07-23 15:36:49 -0700

    fix: `xmake.lua`

--- xmake.lua
+++ xmake.lua
@@ -1,3 +1,20 @@
+set_xmakever("2.8.2")
+
+-- set project
+set_project("commonlibsf")
+set_arch("x64")
+set_languages("c++23")
+set_optimize("faster")
+set_warnings("allextra")
+set_encodings("utf-8")
+
+-- add rules
+add_rules("mode.debug", "mode.release")
+
+-- require packages
+add_requires("xbyak")
+add_requires("spdlog", { configs = { header_only = false, std_format = true } })
+
 -- make extras available
 includes("xmake-extra.lua")
 
