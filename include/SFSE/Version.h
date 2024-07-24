fb1bd8e111f6bd72b122a98b697c15ac6d420834
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-07-23 17:27:52 -0700

    chore: update `SFSE/Version.h`

--- include/SFSE/Version.h
+++ include/SFSE/Version.h
@@ -15,7 +15,8 @@ namespace SFSE
 	constexpr REL::Version RUNTIME_SF_1_10_32(1, 10, 32, 0);
 	constexpr REL::Version RUNTIME_SF_1_11_36(1, 11, 36, 0);
 	constexpr REL::Version RUNTIME_SF_1_12_30(1, 12, 30, 0);
-	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_12_30;
+	constexpr REL::Version RUNTIME_SF_1_12_36(1, 12, 36, 0);
+	constexpr auto         RUNTIME_LATEST = RUNTIME_SF_1_12_36;
 
-	constexpr REL::Version SFSE_PACK_LATEST(0, 2, 8, 0);
+	constexpr REL::Version SFSE_PACK_LATEST(0, 2, 10, 0);
 }
