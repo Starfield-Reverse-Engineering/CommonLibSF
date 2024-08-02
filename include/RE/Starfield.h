4b449aea8c2d26b7b83608ade52c8b95f1df8853
DeweySalt <76853940+Deweh@users.noreply.github.com>
2024-08-01 23:17:30 -0400

    fix `BSAnimationGraphManager`
    add: `BSTSmallArray` stub
    add `AnimationManager`
    add `BSAnimationGraph`

--- include/RE/Starfield.h
+++ include/RE/Starfield.h
@@ -182,6 +182,7 @@
 #include "RE/B/BGSWeatherSettingsForm.h"
 #include "RE/B/BGSWwiseEventForm.h"
 #include "RE/B/BGSWwiseKeywordMapping.h"
+#include "RE/B/BSAnimationGraph.h"
 #include "RE/B/BSBlendable.h"
 #include "RE/B/BSColorCurve.h"
 #include "RE/B/BSContainer.h"
