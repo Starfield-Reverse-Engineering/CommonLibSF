aa98280c7deb12999250f4890cd59f5e887ac303
LarannKiar <178349660+LarannKiar@users.noreply.github.com>
2024-08-14 01:19:21 -0700

    Update Actor.cpp

--- src/RE/A/Actor.cpp
+++ src/RE/A/Actor.cpp
@@ -8,4 +8,10 @@ namespace RE
 		REL::Relocation<func_t> func{ ID::Actor::IsHostileToActor };
 		return func(this, a_actor);
 	}
+	void Actor::SetHelmetFlashlight(bool a_on)
+	{
+		using func_t = decltype(&Actor::SetHelmetFlashlight);
+		REL::Relocation<func_t> func{ REL::ID(153762) };
+		func(this, a_on);
+	}
 }
