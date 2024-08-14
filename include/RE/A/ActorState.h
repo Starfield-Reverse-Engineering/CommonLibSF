421116d34b13738b26cf0941a8d11d3e4b3acb2c
LarannKiar <178349660+LarannKiar@users.noreply.github.com>
2024-08-14 02:51:10 -0700

    Update ActorState.h

--- include/RE/A/ActorState.h
+++ include/RE/A/ActorState.h
@@ -4,6 +4,23 @@
 
 namespace RE
 {
+
+	struct ActorState1
+	{
+	public:
+		// members
+		std::uint32_t     movingBack: 1;        // 0:00
+		std::uint32_t     movingForward: 1;     // 0:01
+		std::uint32_t     movingRight: 1;       // 0:02
+		std::uint32_t     movingLeft: 1;        // 0:03
+		std::uint32_t     unk04: 2;             // 0:04
+		std::uint32_t     walking: 1;           // 0:06
+		std::uint32_t     running: 1;           // 0:07
+		std::uint32_t     sprinting: 1;         // 0:08
+		std::uint32_t     sneaking: 1;          // 0:09
+		std::uint32_t     swimming: 1;          // 0:10
+	};
+
 	class ActorState : public IMovementState
 	{
 	public:
@@ -18,7 +35,7 @@ namespace RE
 		virtual void Unk_34();  // 34
 
 		// members
-		std::uint32_t actorState1;  // 08
+		ActorState1 actorState1;    // 08
 		std::uint32_t actorState2;  // 0C
 	};
 	static_assert(sizeof(ActorState) == 0x10);
