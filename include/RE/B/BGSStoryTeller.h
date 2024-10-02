41d748528051abb35b467caac23954056378054f
Qudix <17361645+Qudix@users.noreply.github.com>
2024-10-02 09:04:14 -0500

    feat: add `BGSStoryTeller`

---
+++ include/RE/B/BGSStoryTeller.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "RE/B/BSTArray.h"
+
+namespace RE
+{
+	class TESQuest;
+
+	class __declspec(novtable) BGSStoryTeller
+	{
+	public:
+		SF_RTTI_VTABLE(BGSStoryTeller);
+
+		[[nodiscard]] static BGSStoryTeller* GetSingleton()
+		{
+			static REL::Relocation<BGSStoryTeller**> singleton{ REL::ID(878850) };
+			return *singleton;
+		}
+
+		// members
+		std::byte           pad00[0x38];
+		BSTArray<TESQuest*> queuedStartQuests;
+		BSTArray<TESQuest*> runningQuests;
+		BSTArray<TESQuest*> queuedStopQuests;
+		std::byte           pad70[0x10];
+		BSTArray<TESQuest*> unk78;
+		BSTArray<TESQuest*> unk88;
+	};
+	static_assert(offsetof(BGSStoryTeller, queuedStartQuests) == 0x38);
+	static_assert(offsetof(BGSStoryTeller, runningQuests) == 0x48);
+	static_assert(offsetof(BGSStoryTeller, queuedStopQuests) == 0x58);
+	static_assert(offsetof(BGSStoryTeller, unk78) == 0x78);
+	static_assert(offsetof(BGSStoryTeller, unk88) == 0x88);
+}
