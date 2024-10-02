8607ded77224a6cc1adb7e1be6f5d85bc840f13a
Qudix <17361645+Qudix@users.noreply.github.com>
2024-10-02 09:00:57 -0500

    feat: `Main`

--- include/RE/M/Main.h
+++ include/RE/M/Main.h
@@ -1,28 +1,47 @@
 #pragma once
 
+#include "RE/B/BSTEvent.h"
+
 namespace RE
 {
 	class NiAVObject;
 	class NiCamera;
 
-	class Main
+	struct PositionPlayerEvent;
+
+	class Main :
+		public BSTEventSink<PositionPlayerEvent>
 	{
 	public:
+		SF_RTTI_VTABLE(Main);
+
 		struct SceneGraphRoot
 		{
-			std::byte   unk[120];
-			NiAVObject* worldCameraRoot;  //NiNode
-			NiCamera*   worldCamera;
+			// members
+			std::byte   pad00[0x78];     // 00
+			NiAVObject* worldCameraRoot; // 78 - NiNode
+			NiCamera*   worldCamera;     // 80
 		};
 		// static_assert(offsetof(SceneGraphRoot, SceneGraphRoot::worldCamera) == 0x80); // FIXME: clang-cl chokes on this assertion
 
-		static SceneGraphRoot* WorldRoot()
+		virtual ~Main();  // 00
+
+		// override
+		virtual BSEventNotifyControl ProcessEvent(const PositionPlayerEvent& a_event, BSTEventSource<PositionPlayerEvent>* a_source) override;  // 01
+
+		[[nodiscard]] static Main* GetSingleton()
 		{
-			static REL::Relocation<SceneGraphRoot**> singleton{ REL::ID(887308) };
+			static REL::Relocation<Main**> singleton{ REL::ID(881027) };
 			return *singleton;
 		}
 
-		static NiCamera* WorldCamera()
+		[[nodiscard]] static SceneGraphRoot* WorldRoot()
+		{
+			static REL::Relocation<SceneGraphRoot**> worldRoot{ REL::ID(887308) };
+			return *worldRoot;
+		}
+
+		[[nodiscard]] static NiCamera* WorldRootCamera()
 		{
 			return WorldRoot()->worldCamera;
 		}
