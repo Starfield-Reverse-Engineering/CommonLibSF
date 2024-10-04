394797d97557d82901ec39d826e97394710d6f3a
Qudix <17361645+Qudix@users.noreply.github.com>
2024-10-04 17:06:59 -0500

    feat: `IAnimationGraphManagerHolder`

--- include/RE/I/IAnimationGraphManagerHolder.h
+++ include/RE/I/IAnimationGraphManagerHolder.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include "RE/B/BSFixedString.h"
+#include "RE/B/BSTSmartPointer.h"
+
 namespace RE
 {
+	class BSAnimationGraphManager;
+
 	class __declspec(novtable) IAnimationGraphManagerHolder
 	{
 	public:
@@ -10,29 +15,30 @@ namespace RE
 		virtual ~IAnimationGraphManagerHolder();  // 00
 
 		// add
-		virtual void Unk_01();  // 01
-		virtual void Unk_02();  // 02
-		virtual void Unk_03();  // 03
-		virtual void Unk_04();  // 04
-		virtual void Unk_05();  // 05
-		virtual void Unk_06();  // 06
-		virtual void Unk_07();  // 07
-		virtual void Unk_08();  // 08
-		virtual void Unk_09();  // 09
-		virtual void Unk_0A();  // 0A
-		virtual void Unk_0B();  // 0B
-		virtual void Unk_0C();  // 0C
-		virtual void Unk_0D();  // 0D
-		virtual void Unk_0E();  // 0E
-		virtual void Unk_0F();  // 0F
-		virtual void Unk_10();  // 10
-		virtual void Unk_11();  // 11
-		virtual void Unk_12();  // 12
-		virtual void Unk_13();  // 13
-		virtual void Unk_14();  // 14
-		virtual void Unk_15();  // 15
-		virtual void Unk_16();  // 16
-		virtual void Unk_17();  // 17
-		virtual void Unk_18();  // 18
+		virtual bool          NotifyAnimationGraphImpl(const BSFixedString& a_eventName);                                                                         // 01
+		virtual void          Unk_02();                                                                                                                           // 02
+		virtual void          Unk_03();                                                                                                                           // 03
+		virtual void          Unk_04();                                                                                                                           // 04
+		virtual void          Unk_05();                                                                                                                           // 05
+		virtual void          Unk_06();                                                                                                                           // 06
+		virtual void          Unk_07();                                                                                                                           // 07
+		virtual void          Unk_08();                                                                                                                           // 08
+		virtual void          Unk_09();                                                                                                                           // 09
+		virtual void          Unk_0A();                                                                                                                           // 0A
+		virtual void          Unk_0B();                                                                                                                           // 0B
+		virtual void          Unk_0C();                                                                                                                           // 0C
+		virtual void          Unk_0D();                                                                                                                           // 0D
+		virtual std::uint32_t GetGraphVariableCacheSize() const { return 0; }                                                                                     // 0E
+		virtual void          Unk_0F();                                                                                                                           // 0F
+		virtual void          Unk_10();                                                                                                                           // 10
+		virtual void          Unk_11();                                                                                                                           // 11
+		virtual bool          GetGraphVariableImplFloat(const BSFixedString& a_variable, float& a_out) const;                                                     // 12
+		virtual bool          GetGraphVariableImplInt(const BSFixedString& a_variable, std::int32_t& a_out) const;                                                // 13
+		virtual bool          GetGraphVariableImplBool(const BSFixedString& a_variable, bool& a_out) const;                                                       // 14
+		virtual void          PreUpdateAnimationGraphManager([[maybe_unused]] const BSTSmartPointer<BSAnimationGraphManager>& a_animGraphMgr) const { return; }   // 15
+		virtual void          PostUpdateAnimationGraphManager([[maybe_unused]] const BSTSmartPointer<BSAnimationGraphManager>& a_animGraphMgr) const { return; }  // 16
+		virtual void          PreLoadAnimationGraphManager([[maybe_unused]] const BSTSmartPointer<BSAnimationGraphManager>& a_animGraphMgr) { return; }           // 17
+		virtual void          PostLoadAnimationGraphManager([[maybe_unused]] const BSTSmartPointer<BSAnimationGraphManager>& a_animGraphMgr) { return; }          // 18
 	};
+	static_assert(sizeof(IAnimationGraphManagerHolder) == 0x8);
 }
