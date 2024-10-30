420f705ed479fe7dd35c4d3c701ca9b8fa2faf70
Qudix <17361645+Qudix@users.noreply.github.com>
2024-10-30 17:45:44 -0500

    feat: `TESPackage`

--- include/RE/T/TESPackage.h
+++ include/RE/T/TESPackage.h
@@ -4,13 +4,51 @@
 
 namespace RE
 {
-	class TESPackage : public TESForm
+	class TESPackageData;
+
+	struct PACKAGE_DATA
+	{
+		// members
+		std::uint32_t packageFlags;           // 0
+		std::int8_t   packageType;            // 4
+		std::int8_t   interruptOverrideType;  // 5
+		std::int8_t   maxSpeed;               // 6
+		std::uint16_t foBehaviorFlags;        // 8
+		std::uint16_t packageSpecificFlags;   // A
+	};
+	static_assert(sizeof(PACKAGE_DATA) == 0xC);
+
+	struct PackageLocation
+	{
+
+	};
+
+	struct PackageTarget
+	{
+		std::uint8_t type;
+		union
+		{
+			BSPointerHandle<TESObjectREFR> refHandle;
+			TESForm*                       object;
+		};
+		std::int32_t value;
+	};
+	static_assert(sizeof(PackageTarget) == 0x18);
+
+	class __declspec(novtable) TESPackage :
+		public TESForm  // 00
 	{
 	public:
 		SF_RTTI_VTABLE(TESPackage);
 		SF_FORMTYPE(PACK);
 
 		~TESPackage() override;  // 00
+
+		// members
+		PACKAGE_DATA     packageData;      // 38
+		TESPackageData*  packageData2;     // 48
+		PackageLocation* packageLocation;  // 50
+		PackageTarget*   packageTarget;    // 58
 	};
-	//static_assert(sizeof(TESPackage) == 0xE8);
+	static_assert(offsetof(TESPackage, packageData2) == 0x48);
 }
