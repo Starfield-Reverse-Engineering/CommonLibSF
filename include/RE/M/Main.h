1a184dc5b574c64cf98c6b791a60dc7de20f4e3e
Qudix <17361645+Qudix@users.noreply.github.com>
2024-11-01 19:16:00 -0500

    feat: `Main`

--- include/RE/M/Main.h
+++ include/RE/M/Main.h
@@ -47,8 +47,14 @@ namespace RE
 		}
 
 		// members
-		std::byte pad008[0x440];     // 008
+		std::byte pad008[0x020];     // 008
+		bool      quitGame;          // 028
+		std::byte pad029[0x005];     // 029
+		bool      resetGame;         // 02E
+		std::byte pad02F[0x419];     // 02F
 		bool      isGameMenuPaused;  // 448
 	};
+	static_assert(offsetof(Main, quitGame) == 0x028);
+	static_assert(offsetof(Main, resetGame) == 0x02E);
 	static_assert(offsetof(Main, isGameMenuPaused) == 0x448);
 }
